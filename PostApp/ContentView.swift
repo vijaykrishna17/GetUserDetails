919d60faa015752c75d502f6a216717bc1f21713
vijay.gudipati <vijay.gudipati@trianz.com>
2022-09-28 16:27:02 +0530

    Initial Commit

---
+++ PostApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PostApp
+//
+//  Created by Trianz on 06/07/1944 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PostApp/PostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAppApp.swift
+//  PostApp
+//
+//  Created by Trianz on 06/07/1944 Saka.
+//
+
+import SwiftUI
+
+@main
+struct PostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
